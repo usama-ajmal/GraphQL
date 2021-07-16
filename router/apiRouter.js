7b2c7b26b4e13d0629a95203677bdc353ce5cf12
Usama Ajmal <usama.ajmal@cloudtek.biz>
2021-07-16 12:34:45 -0400

    calculate route and controller

--- router/apiRouter.js
+++ router/apiRouter.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const router = express.Router();
-
+const apiController = require('./../controller/apiController')
 
 router.get('/',(req,res)=>{
     res.send('Hello World!')
+})
+
+router.get('/calculate/:value1/:value2',(req,res)=>{
+    const a = parseInt(req.params.value1);
+    const b = parseInt(req.params.value2);
+
+    const result = apiController.calculate2values(a,b)
+
+    res.send(`${a} + ${b} = ${result}`)
 })
 
 module.exports = router;
